d9585fdb8fbea3340695a6b716df50d1eb9b6ca5
GAEL_ROGELIO_BEJARANO_DOMINGUEZ <a.22308051281154@cbtis128.edu.mx>
2025-03-12 19:01:38 +0000

    Mangos Mangos 💀🥭🔥

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,85 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class Membresia {
+  final String idMem, idCliente;
+  final double costo;
+  Membresia(this.idMem, this.idCliente, this.costo);
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MembresiasScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MembresiasScreen extends StatelessWidget {
+  final membresias = [
+    Membresia('M001', 'C001', 100.0),
+    Membresia('M002', 'C002', 150.0),
+    Membresia('M003', 'C003', 200.0),
+    Membresia('M004', 'C004', 250.0),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Bejarano Examen 1154',
+            style: TextStyle(fontSize: 26, color: Colors.white)),
+        backgroundColor: Color(0xff4b96db),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(20),
+        itemCount: membresias.length,
+        itemBuilder: (context, index) {
+          final membresia = membresias[index];
+          return Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: _getColor(index),
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.7),
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('ID Membresía: ${membresia.idMem}',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Text('ID Cliente: ${membresia.idCliente}',
+                    style: TextStyle(fontSize: 16)),
+                Text('Costo: \$${membresia.costo.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 16)),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (_, __) => SizedBox(height: 16),
       ),
     );
+  }
+
+  Color _getColor(int index) {
+    return [
+      Colors.blue.shade100,
+      Colors.green.shade100,
+      Colors.orange.shade100,
+      Colors.purple.shade100
+    ][index % 4];
   }
 }
